089c88ea5dadcacce3532021c8ceead4c0067e1e
Jubbery <jackroberts28077@gmail.com>
2022-11-17 20:19:39 -0500

    Removed unnecessary code

--- robot.cpp
+++ robot.cpp
@@ -8,7 +8,6 @@ using namespace std;
 int main() {
     ifstream robotText;
     robotText.open("input.txt");
-    // deque<string> fileText;
     vector<int> sprockets;
     vector<int> prev;
     int s;
@@ -17,33 +16,23 @@ int main() {
     int stages;
     if (robotText.is_open()) {
         getline(robotText, operation);
-        string tmp;
-        getline(robotText, tmp);
-        stages = stoi(tmp);
-        while (!robotText.eof()) {
-            string sp;
-            getline(robotText, sp);
-            // fileText.push_back(sp);
-            sscanf(sp.c_str(), "%d %d", &s, &p);
-            sprockets.push_back(s);
-            prev.push_back(p);
+        if (operation[0] == 'r') {
+            string tmp;
+            getline(robotText, tmp);
+            stages = stoi(tmp);
+            while (!robotText.eof()) {
+                string sp;
+                getline(robotText, sp);
+                sscanf(sp.c_str(), "%d %d", &s, &p);
+                sprockets.push_back(s);
+                prev.push_back(p);
+            }
         }
-        // fileText.pop_back();
+        else if (operation[0] == 'o') {
+            cout << "omnidroid";
+        }
         robotText.close();
     }
-    // string operation = fileText[0];
-    // int stages = stoi(fileText[1]);
-    // fileText.pop_front();
-    // fileText.pop_front();
-    // for (auto x : fileText) {
-    //     int whitespace = x.find(" ");
-    //     int n = x.length();
-    //     s = stoi(x.substr(0, whitespace));
-    //     p = stoi(x.substr(whitespace, n - 1));
-    //     sscanf(x.c_str(), "%d %d", &s, &p);
-    //     sprockets.push_back(s);
-    //     prev.push_back(p);
-    // }
 
 
 
@@ -74,6 +63,7 @@ int main() {
 }
 
 int robotomaton() {
+
     return 0;
 }
 
@@ -82,9 +72,9 @@ int omnidroid() {
 }
 
 // for (auto x : sprockets) {
-        //     cout << x << ' ';
-        // }
-        // cout << '\n';
-        // for (auto x : prev) {
-        //     cout << x << ' ';
-        // }
+//     cout << x << ' ';
+// }
+// cout << '\n';
+// for (auto x : prev) {
+//     cout << x << ' ';
+// }
