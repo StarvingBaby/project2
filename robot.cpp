d8b39c4df1f61b76671d852fe478c74ff72b2fcf
Jubbery <jackroberts28077@gmail.com>
2022-11-19 00:30:59 -0500

    Clean-up

--- robot.cpp
+++ robot.cpp
@@ -6,20 +6,14 @@
 using namespace std;
 
 vector<int> sprockets;
-vector<int> previous;
-int numRobots;
-int s;
-int p;
-string operation = "";
-int stages;
 
 int robotomaton(int p, int i) {
-    cout << sprockets[i] <<  "";
+    cout << sprockets[i] << "";
     if (p == 0) {
         return sprockets[i];
     }
-    cout <<  " + ";
-    return sprockets[i] + robotomaton(p-1, i-1);
+    cout << " + ";
+    return sprockets[i] + robotomaton(p - 1, i - 1);
 
     // return;
 }
@@ -30,6 +24,12 @@ int omnidroid() {
 
 int main() {
     ifstream robotText;
+    vector<int> previous;
+    int numRobots;
+    int s;
+    int p;
+    string operation = "";
+    int stages;
     robotText.open("input.txt");
     if (robotText.is_open()) {
         string tmp;
@@ -57,39 +57,22 @@ int main() {
         robotText.close();
     }
 
-
-
-    int sum = 0;
     if (operation[0] == 'o') {
         cout << "omnidroid";
     }
     else if (operation[0] == 'r') {
-        for (auto x : sprockets) {
-            cout << x << ' ';
-        }
-        cout << '\n';
-        for (auto x : previous) {
-            cout << x << ' ';
-        }
-        cout << "\n\n";
-        vector<int> tmpArray(stages);
         for (int i = 0; i < stages; i++) {
-            int tmpSum = 0;
             if (previous[i] == 0) {
-                cout << "prev = " << previous[i] << "   i = "<< i << " --->  "<< " = " << sprockets[i] <<'\n';
+                cout << "prev = " << previous[i] << "   i = " << i << " --->  " << " = " << sprockets[i] << '\n';
                 sprockets[i] = sprockets[i];
             }
             else {
-                cout << "prev = " << previous[i] << "   i = "<< i << " --->  ";
+                cout << "prev = " << previous[i] << "   i = " << i << " --->  ";
                 sprockets[i] = robotomaton(previous[i], i);
-                cout << " = " << sprockets[i] <<'\n';
+                cout << " = " << sprockets[i] << '\n';
             }
         }
-        for (int i = 0; i < stages; i++) {
-            sum = sprockets[i];
-            cout << sprockets[i] << ' ';
-        }
-        cout << '\n' << sum << '\n';
+        cout << sprockets[stages - 1] << '\n';
     }
     else {
         return 1;
