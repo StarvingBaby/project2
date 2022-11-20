fe581ab6cd26be86b4949ef88160f75e17585d17
Jubbery <jackroberts28077@gmail.com>
2022-11-20 14:51:22 -0500

    Write answers to 'output.txt'

--- robot.cpp
+++ robot.cpp
@@ -8,11 +8,11 @@ using namespace std;
 vector<int> sprockets;
 
 int robotomaton(int p, int i) {
-    cout << sprockets[i] << "";
+    // cout << sprockets[i] << "";
     if (p == 0) {
         return sprockets[i];
     }
-    cout << " + ";
+    // cout << " + ";
     return sprockets[i] + robotomaton(p - 1, i - 1);
 }
 
@@ -21,7 +21,8 @@ int omnidroid() {
 }
 
 int main() {
-    ifstream robotText;
+    ifstream inputText;
+    ofstream outputText;
     vector<int> previous;
     int numRobots;
     int s;
@@ -30,60 +31,66 @@ int main() {
     int m;
     string operation = "";
     int stages;
-    robotText.open("input.txt");
-    if (robotText.is_open()) {
+    inputText.open("input.txt");
+    outputText.open("output.txt");
+    if (inputText.is_open()) {
         string tmp;
-        getline(robotText, tmp);
+        getline(inputText, tmp);
         numRobots = stoi(tmp);
         for (int i = 0; i < numRobots; i++) {
-            getline(robotText, operation);
+            getline(inputText, operation);
             while (operation == "") {
-                getline(robotText, operation);
+                getline(inputText, operation);
             }
             if (operation[0] == 'r') {
-                getline(robotText, tmp);
+                getline(inputText, tmp);
                 stages = stoi(tmp);
                 string sp;
                 for (int i = 0; i < stages; i++) {
-                    getline(robotText, sp);
+                    getline(inputText, sp);
                     sscanf(sp.c_str(), "%d %d", &s, &p);
                     sprockets.push_back(s);
                     previous.push_back(p);
                 }
                 for (int i = 0; i < stages; i++) {
                     if (previous[i] == 0) {
-                        cout << "prev = " << previous[i] << "   i = " << i << " --->  " << " = " << sprockets[i] << '\n';
+                        // cout << "prev = " << previous[i] << "   i = " << i << " --->  " << " = " << sprockets[i] << '\n';
                         sprockets[i] = sprockets[i];
                     }
                     else {
-                        cout << "prev = " << previous[i] << "   i = " << i << " --->  ";
+                        // cout << "prev = " << previous[i] << "   i = " << i << " --->  ";
                         sprockets[i] = robotomaton(previous[i], i);
-                        cout << " = " << sprockets[i] << '\n';
+                        // cout << " = " << sprockets[i] << '\n';
                     }
                 }
-                cout << sprockets[stages - 1] << '\n';
+                // cout << sprockets[stages - 1] << '\n';
+                outputText << sprockets[stages - 1] << '\n';
+                sprockets.clear();
+                previous.clear();
             }
             else if (operation[0] == 'o') {
                 string nm, tmp;
                 int x, y;
-                getline(robotText, nm);
-                cout << nm << '\n';
+                getline(inputText, nm);
+                // cout << nm << '\n';
                 sscanf(nm.c_str(), "%d %d", &n, &m);
                 for (int i = 0; i < m; i++) {
-                    getline(robotText, tmp);
-                    cout << tmp << '\n';
+                    getline(inputText, tmp);
+                    // cout << tmp << '\n';
                     sscanf(tmp.c_str(), "%d %d", &x, &y);
                     // use x and y to load arrays
                 }
                 for (int i = 0; i < n; i++) {
-                    getline(robotText, tmp);
-                    cout << tmp << '\n';
+                    getline(inputText, tmp);
+                    // cout << tmp << '\n';
                     sscanf(tmp.c_str(), "%d", &x);
                     // use x to load sprockets array
                 }
+                outputText << "... omni answer ..." << '\n';
                 continue;
             }
         }
-        robotText.close();
+        inputText.close();
+        outputText.close();
     }
 }
